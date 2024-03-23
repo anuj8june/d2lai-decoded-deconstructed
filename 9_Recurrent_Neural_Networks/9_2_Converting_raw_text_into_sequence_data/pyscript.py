cea99d692a501c5970449eb6b43b5e382ba33b21
Anuj Anand Gagrai <anuj8june@gmail.com>
2024-03-23 13:09:13 +0530

    zipf's law and exercise complete

--- 9_Recurrent_Neural_Networks/9_2_Converting_raw_text_into_sequence_data/pyscript.py
+++ 9_Recurrent_Neural_Networks/9_2_Converting_raw_text_into_sequence_data/pyscript.py
@@ -1,6 +1,9 @@
 import re
 import requests
 import collections
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 
 class DataPreprocessingPipeline(object):
@@ -67,6 +70,50 @@ class Vocabulary(object):
         return self.token_to_idx["<unk>"]
 
 
+def calculate_word_freq_indices(vocab_object):
+    word_frequency = []
+    token_position = []
+    tokens = []
+    token_position_count = 1
+    for token, freq in vocab_object.token_freqs:
+        word_frequency.append(freq)
+        tokens.append(token)
+        token_position.append(token_position_count)
+        token_position_count += 1
+
+    word_frequency_log = [np.log(wf) for wf in word_frequency]
+    token_position_log = [np.log(tp) for tp in token_position]
+
+    return word_frequency_log, token_position_log
+
+
+def ziphs_law_plot(vocab_object, label, plot_name):
+    if not isinstance(vocab_object, list):
+        word_frequency_log, token_position_log = calculate_word_freq_indices(
+                                                vocab_object)        
+
+        plt.plot(token_position_log, word_frequency_log, label=label)
+        plt.ylabel('frequency: n(x)')
+        plt.xlabel('token: x')
+        if label is not None:
+            plt.legend()
+        plt.savefig(plot_name)
+    else:
+        for v, l, p in zip(vocab_object, label, plot_name):
+            ziphs_law_plot(v, l, p)
+
+    
+def calculate_params_for_zipf(vocab_objects):
+    for vocab_label, vocab_object in vocab_objects.items():
+        word_frequency_log, token_position_log = calculate_word_freq_indices(
+                                                vocab_object)
+        X = np.reshape(np.array(word_frequency_log), (-1,1))
+        y = np.array(token_position_log)
+        lr = LinearRegression()
+        lr.fit(X=X, y=y)
+        print(f"Coefficient for {vocab_label} is : {lr.coef_}")
+
+
 
 if __name__ == "__main__":
 
@@ -90,4 +137,79 @@ if __name__ == "__main__":
     print(f"\nindices:\n{indices}")
     print(f"\nwords:\n{vocab.to_tokens(indices)}")
 
+    # Language statistics word level for unigrams
+    words = text.split()
+    unigram_vocab = Vocabulary(words)
+    print(f"\nTop 10 token frequencies:\n{unigram_vocab.token_freqs[:10]}")
+    ziphs_law_plot(vocab_object=unigram_vocab, label=None,
+                   plot_name="zipfs_law_word_freq_log_idx_log_unigram.jpg")
     
+    # Language statistics word level for bigrams
+    bigram_tokens = ['--'.join(pair) for pair in zip(words[:-1], words[1:])]
+    bigram_vocab = Vocabulary(bigram_tokens)
+    print(f"\nBigram words token frequency:\n{bigram_vocab.token_freqs[:10]}")
+
+    # Language statistics word level for trigrams
+    trigram_tokens = ['--'.join(pair) for pair in zip(words[:-2], words[1:-1], 
+                                                      words[2:])]
+    trigram_vocab = Vocabulary(trigram_tokens)
+    print(f"\nTrigram words token frequency:\n{trigram_vocab.token_freqs[:10]}")
+    
+    ziphs_law_plot(vocab_object=[unigram_vocab, bigram_vocab, trigram_vocab], 
+                   label=["unigram", "bigram", "trigram"], 
+                   plot_name=["zipfs_law_word_freq_log_idx_log.jpg"]*3)
+    
+
+    ### Excercise ###
+    # 1. In the experiment of this section, tokenize text into words and vary 
+    # the min_freq argument value of the Vocab instance. Qualitatively 
+    # characterize how changes in min_freq impact the size of the resulting 
+    # vocabulary.
+    min_freqs = [1,2,3,4,5]
+    for min_freq in min_freqs:
+        words = text.split()
+        vocab = Vocabulary(words, min_freq=min_freq)
+        print(f"Vocab size for min frequency: {min_freq} is {len(vocab)}")
+
+    # 2. Estimate the exponent of Zipfian distribution for unigrams, bigrams, 
+    # and trigrams in this corpus.
+    params = {
+        "unigram": unigram_vocab,
+        "bigram": bigram_vocab,
+        "triigram": trigram_vocab,
+    }
+    calculate_params_for_zipf(vocab_objects=params)
+
+    # 3. Find some other sources of data (download a standard machine learning 
+    # dataset, pick another public domain book, scrape a website, etc). For each, 
+    # tokenize the data at both the word and character levels. How do the 
+    # vocabulary sizes compare with The Time Machine corpus at equivalent values 
+    # of min_freq. Estimate the exponent of the Zipfian distribution 
+    # corresponding to the unigram and bigram distributions for these corpora. 
+    # How do they compare with the values that you observed for 
+    # The Time Machine corpus?
+    
+    # The haunter of the dark by H. P. Lovecraft from 
+    # https://www.gutenberg.org/ebooks/73233
+    link = "https://www.gutenberg.org/cache/epub/73233/pg73233.txt"
+    data_dump_path = "data_ex3.txt"
+    dpp.download_data(link, data_dump_path)
+    text = dpp.read_file_contents(data_dump_path)
+    text = dpp.preprocess(text)
+
+    words = text.split()
+    unigram_vocab = Vocabulary(words)
+    bigram_tokens = ['--'.join(pair) for pair in zip(words[:-1], words[1:])]
+    bigram_vocab = Vocabulary(bigram_tokens)
+
+    min_freqs = [1,2,3,4,5]
+    for min_freq in min_freqs:
+        words = text.split()
+        vocab = Vocabulary(words, min_freq=min_freq)
+        print(f"Vocab size for min frequency: {min_freq} is {len(vocab)}")
+
+    params = {
+        "unigram": unigram_vocab,
+        "bigram": bigram_vocab,
+    }
+    calculate_params_for_zipf(vocab_objects=params)
